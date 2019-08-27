b9ee58f73d04f7087fb86cc592e68dee812d39a1
Nina Withers <ninarosewithers@gmail.com>
2019-08-27 08:40:55 -0400

    added dependencies

---
+++ client/src/actions/itemActions.js
@@ -0,0 +1,17 @@
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from "./types";
+
+export const getItems = () => {
+  return {
+    type: GET_ITEMS
+  };
+//   export const addItems = () => {
+//     return {
+//       type: ADD_ITEMS
+//     };
+//   };
+//   export const deleteItems = () => {
+//     return {
+//       type: DELETE_ITEMS
+//     };
+//   };
+};
